7ee20616c6ebc630e0117f643d2afe4bbb35ae3e
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-04-01 00:52:35 +0900

    :fire: (examples) Remove unused

--- examples/vue/src/main.ts
+++ examples/vue/src/main.ts
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
-import plugin, { useSort } from '@miyauci/vue-data-table'
+import plugin from '@miyauci/vue-data-table'
 import App from './App.vue'
 
-import '../../../packages/vue/dist/style.css'
 createApp(App).use(plugin()).mount('#app')
