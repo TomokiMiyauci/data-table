d5dd368f3165aade6fecca56e2112c6bf173e067
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-03-31 22:22:16 +0900

    :recycle: (vue) Refactor useFilter hooks and test it

--- packages/vue/src/hooks/useFilter.ts
+++ packages/vue/src/hooks/useFilter.ts
@@ -10,6 +10,75 @@ type Options = {
   headers: Ref<Header[]>
   items: Ref<Item[]>
 }
+type HeaderValueAndFilter = Pick<Header, 'value' | 'filter'>
+type StringOrNumber = string | number
+type GetFilteredItems = {
+  filterableHeaderValues: ComputedRef<HeaderValueAndFilter[]>
+  search: Ref<StringOrNumber>
+  items: Ref<Item[]>
+}
+
+const filterUndefinedOrTrue = ({ filterable }: Header): boolean =>
+  isUndefinedOrTrue(filterable)
+const mapValueFilter = ({ value, filter }: Header): HeaderValueAndFilter => ({
+  value,
+  filter
+})
+const getFilterableHeaderValues = (
+  headers: Ref<Header[]>
+): HeaderValueAndFilter[] =>
+  headers.value.filter(filterUndefinedOrTrue).map(mapValueFilter)
+
+const hasStringLetter = (val: string, search: StringOrNumber): boolean =>
+  val.includes(toStringOrNumber(search, 'string'))
+const hasNumberLetter = (val: number, search: StringOrNumber): boolean => {
+  if (typeof search === 'string' && search === '') return true
+  return val === search
+}
+const hasDateLetter = (val: Date, search: StringOrNumber): boolean =>
+  val.toLocaleString().includes(toStringOrNumber(search, 'string'))
+const someCheck = ({
+  item,
+  search
+}: {
+  item: Item
+  search: Ref<StringOrNumber>
+}) => ({ filter, value }: HeaderValueAndFilter): boolean => {
+  const _value = item[value]
+  if (typeof filter === 'function') {
+    return filter(_value, toStringOrNumber(search.value, 'string'))
+  }
+  if (typeof _value === 'string') {
+    return hasStringLetter(_value, search.value)
+  } else if (typeof _value === 'number') {
+    return hasNumberLetter(_value, search.value)
+  } else if (_value instanceof Date) {
+    return hasDateLetter(_value, search.value)
+  } else return true
+}
+
+const filterItem = ({
+  filterableHeaderValues,
+  search
+}: {
+  filterableHeaderValues: ComputedRef<HeaderValueAndFilter[]>
+  search: Ref<StringOrNumber>
+}) => (item: Item): boolean =>
+  filterableHeaderValues.value.some(
+    someCheck({
+      item,
+      search
+    })
+  )
+
+const getFilteredItems = ({
+  filterableHeaderValues,
+  search,
+  items
+}: GetFilteredItems): Item[] => {
+  if (!search.value || !filterableHeaderValues.value.length) return items.value
+  return items.value.filter(filterItem({ filterableHeaderValues, search }))
+}
 
 const useFilter = ({
   items,
@@ -20,10 +89,7 @@ const useFilter = ({
 } => {
   const _search = ref<string | number>('')
   const filterableHeaderValues = computed<Pick<Header, 'value' | 'filter'>[]>(
-    () =>
-      headers.value
-        .filter(({ filterable }) => isUndefinedOrTrue(filterable))
-        .map(({ value, filter }) => ({ value, filter }))
+    () => getFilterableHeaderValues(headers)
   )
 
   const filter = (search: string | number): void => {
@@ -32,29 +98,13 @@ const useFilter = ({
     _search.value = lowerCaseSearch
   }
 
-  const filteredItems = computed<Item[]>(() => {
-    if (!_search.value || !filterableHeaderValues.value.length)
-      return items.value
-    return items.value.filter((item) =>
-      filterableHeaderValues.value.some(({ value: v, filter }) => {
-        const value = item[v]
-        if (typeof filter === 'function') {
-          return filter(value, toStringOrNumber(_search.value, 'string'))
-        }
-        if (typeof value === 'string') {
-          return value
-            .toLowerCase()
-            .includes(toStringOrNumber(_search.value, 'string'))
-        } else if (typeof value === 'number') {
-          return value === _search.value
-        } else if (value instanceof Date) {
-          return value
-            .toLocaleString()
-            .includes(toStringOrNumber(_search.value, 'string'))
-        }
-      })
-    )
-  })
+  const filteredItems = computed<Item[]>(() =>
+    getFilteredItems({
+      filterableHeaderValues,
+      search: _search,
+      items
+    })
+  )
 
   return {
     items: filteredItems,
@@ -62,4 +112,15 @@ const useFilter = ({
   }
 }
 
-export { useFilter }
+export {
+  filterUndefinedOrTrue,
+  getFilterableHeaderValues,
+  hasDateLetter,
+  hasNumberLetter,
+  hasStringLetter,
+  HeaderValueAndFilter,
+  mapValueFilter,
+  someCheck,
+  StringOrNumber,
+  useFilter
+}
