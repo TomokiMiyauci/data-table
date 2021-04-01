4be98849ceace88f98cd71f7c570cc54c67c0a18
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-04-01 21:33:25 +0900

    :lipstick: (vue) Improve sort button UI

--- packages/vue/src/components/DataTable.vue
+++ packages/vue/src/components/DataTable.vue
@@ -2,9 +2,26 @@
   <table :class="table">
     <thead :class="thead">
       <tr>
-        <th v-for="{ text, value } in headers" :key="value" :class="th">
-          {{ text }}
-          <button @click="onClick(value)">{{ getState(value) }}</button>
+        <th
+          v-for="{ text, value } in headers"
+          :key="value"
+          :class="th"
+          @click="onClick(value)"
+        >
+          <span class="cursor-auto" @click.stop>{{ text }}</span>
+          <button
+            class="rounded-full border w-6 h-6 cursor-pointer outline-none focus:(outline-none) active:(ring-4 ring-blue-500 bg-gray-200) shadow hover:(border-blue-500 shadow-md bg-gray-100 ring-2 ring-blue-200 ) p-1 opacity-50 group-hover:opacity-100 transition duration-200"
+            @click="onClick(value)"
+          >
+            <div
+              :class="[
+                getState(value) === 'DESC' ? 'rotate-180 text-blue-500' : ''
+              ]"
+              class="transform transition duration-500"
+            >
+              <IconAccessibility />
+            </div>
+          </button>
         </th>
       </tr>
     </thead>
@@ -15,24 +32,27 @@
         </td>
       </tr>
     </tbody>
-    <tfoot v-if="pagination">
-      <select v-model="row">
-        <option v-for="num in rows" :key="num" :value="num">{{ num }}</option>
-      </select>
+    <tfoot v-if="pagination" class="p-10 m-4">
+      <tr class="p-3">
+        <th scope="row">
+          <select v-model="row">
+            <option v-for="num in rows" :key="num" :value="num">
+              {{ num }}
+            </option>
+          </select>
 
-      <button :disabled="!canPrev" @click="prev">prev</button>
-      {{
-        page
-      }}-{{
-        pages
-      }}
-      <button :disabled="!canNext" @click="next">next</button>
+          <button :disabled="!canPrev" @click="prev">prev</button>
+          {{ page }}-{{ pages }}
+          <button :disabled="!canNext" @click="next">next</button>
+        </th>
+      </tr>
     </tfoot>
   </table>
 </template>
 
 <script setup lang="ts">
 import type { Header, Item } from '@miyauci/data-table-core'
+import IconAccessibility from 'virtual:vite-icons/carbon/arrow-down'
 import type { PropType } from 'vue'
 import { computed, defineProps, toRefs, watch } from 'vue'
 
@@ -61,7 +81,7 @@ const props = defineProps({
 const table = 'min-w-full divide-y divide-gray-200'
 const thead = 'bg-gray-50 whitespace-nowrap uppercase'
 const th =
-  'p-2 sm:p-3 lg:p-4 text-xs font-medium tracking-wider text-left text-gray-500 uppercase'
+  'group p-2 space-x-3 cursor-pointer sm:p-3 lg:p-4 text-xs font-medium tracking-wider text-left text-gray-500 uppercase'
 const tbody = 'bg-white divide-y divide-gray-200'
 const tr = 'transition text-gray-600 hoverLbg-gray-100 hover:shadow-lg'
 const td = 'px-2 sm:px-4 lg:px-6 py-1 sm:py-2 lg:py-4 whitespace-nowrap'
@@ -87,7 +107,13 @@ const {
 } =
   typeof props.pagination === 'boolean'
     ? ({ items: sortedItems } as Pagination)
-    : usePagination(sortedItems, props.pagination, search)
+    : usePagination(sortedItems, props.pagination)
 
 const onClick = (val: string | number) => sort(val)
 </script>
+
+<style scoped>
+* {
+  @apply antialiased;
+}
+</style>
