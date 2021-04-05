e448c7291fcb41e44955c29818449be969d9f409
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-04-05 16:33:40 +0900

    :memo: (vue) Add app example

--- packages/vue/src/App.vue
+++ packages/vue/src/App.vue
@@ -1,22 +1,59 @@
 <template>
-  <h1>HELLO</h1>
+  <div class="container mx-auto">
+    <div class="text-right mb-4">
+      <input
+        v-model="search"
+        placeholder="search"
+        type="search"
+        class="shadow p-2"
+      />
+    </div>
+    <div
+      class="shadow overflow-hidden transition duration-200 rounded hover:shadow-md"
+    >
+      <DataTable
+        :headers="headers"
+        :items="items"
+        :search="search"
+        :pagination="[2, 3, 'ALL']"
+        :loading="loading"
+      />
+    </div>
+  </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
+<script setup lang="ts">
+import type { Header, Item } from '@miyauci/data-table-core'
+import ky from 'ky'
+import { ref } from 'vue'
 
-export default defineComponent({
-  name: 'App'
-})
+import DataTable from '@/components/DataTable.vue'
+
+const headers: Header[] = [
+  {
+    text: 'Framework',
+    value: 'framework'
+  },
+  {
+    text: 'Platform',
+    value: 'platform'
+  }
+]
+
+const items = ref<Item[]>([])
+const loading = ref<boolean>(false)
+loading.value = true
+ky.get('http://localhost:3004/posts')
+  .json<Item[]>()
+  .then((r) => {
+    items.value = r
+  })
+  .catch((e) => {
+    console.error(e)
+  })
+  .finally(() => {
+    loading.value = false
+  })
+
+const search = ref('')
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
