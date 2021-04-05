6a4fa361a14f847c5f961faef8be5c664f2fbbe1
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-04-05 16:32:24 +0900

    :sparkles: (react) Add useFilter

--- packages/react/src/components/DataTable.tsx
+++ packages/react/src/components/DataTable.tsx
@@ -1,31 +1,43 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect } from 'react'
 import type { Header, Item } from '@miyauci/data-table-core'
+import { useFilter } from '../hooks'
 
 const DataTable: FC<{
   headers: Header[]
   items: Item[]
-}> = ({ headers, items }) => (
-  <table>
-    <thead>
-      <tr>
-        {headers.map(({ text, value }) => (
-          <th key={value}>{text}</th>
-        ))}
-      </tr>
-    </thead>
-    <tbody>
-      {items.map((item, index) => (
-        <tr key={index}>
-          {headers.map(({ value }) => (
-            <td key={value}>{item[value]}</td>
+  search?: string
+}> = ({ headers, items, search }) => {
+  const { filter, items: filteredItems } = useFilter({ items, headers })
+
+  useEffect(() => {
+    filter(search || '')
+  }, [search])
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          {headers.map(({ text, value }) => (
+            <th key={value}>{text}</th>
           ))}
         </tr>
-      ))}
-    </tbody>
-  </table>
-)
+      </thead>
+      <tbody>
+        {filteredItems.map((item, index) => (
+          <tr key={index}>
+            {headers.map(({ value }) => (
+              <td key={value}>{item[value]}</td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  )
+}
 
-DataTable.defaultProps!.headers = []
-DataTable.defaultProps!.items = []
+if (DataTable.defaultProps) {
+  DataTable.defaultProps.headers = []
+  DataTable.defaultProps.items = []
+}
 
 export default DataTable
